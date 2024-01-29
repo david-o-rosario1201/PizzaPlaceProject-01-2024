198bf6bc0a268fb3cd94d1dddefad9be67b72d8d
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-29 15:01:32 -0400

    Agrege la clase Cliente a la carpeta Shared

---
+++ Shared/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaPlace.Shared;
+
+public class Cliente
+{
+	public int Id { get; set; }
+	public string Nombre { get; set; } = default!;
+	public string Calle { get; set; } = default!;
+	public string Ciudad { get; set; } = default!;
+}
