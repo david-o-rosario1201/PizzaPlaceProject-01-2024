9146572e1a4e362031e2b44e948f2866740f5455
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-29 15:04:29 -0400

    Agrege la clase CarritoCompras a la carpeta Shared

---
+++ Shared/CarritoCompras.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaPlace.Shared;
+
+public class CarritoCompras
+{
+	public Cliente Cliente { get; set; } = new Cliente();
+	public List<int> Ordenes { get; set; } = new List<int>();
+	public bool HaPagado { get; set; }
+}
