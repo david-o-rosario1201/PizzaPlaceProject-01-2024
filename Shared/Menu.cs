826ba2382c71bc3d7a380f14b9964bd5004ca4d4
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-29 14:58:52 -0400

    Agrege la clase Menu a la carpeta Shared

---
+++ Shared/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaPlace.Shared;
+
+public class Menu
+{
+	public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+	public void Agregar(Pizza pizza) => Pizzas.Add(pizza);
+	public Pizza? ObtenerPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+}
