223c901a9e3735344a7cc720750322400d17aeac
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-30 11:15:21 -0400

    Agregué la carpeta Client con la clase ExtensionesDepuracion

---
+++ Client/ExtensionesDepuracion.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+
+namespace PizzaPlace.Client;
+
+public static class ExtensionesDepuracion
+{
+	private static JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+	public static string ToJson(this object obj) => JsonSerializer.Serialize(obj, options);
+}
