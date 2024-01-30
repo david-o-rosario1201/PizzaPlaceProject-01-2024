0b92721b1d7c110084c2008ae34838abddacf4ce
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-30 11:13:32 -0400

    Agregue le metodo Agregar y Remover al CarritoCompras

--- Shared/CarritoCompras.cs
+++ Shared/CarritoCompras.cs
@@ -7,4 +7,8 @@ public class CarritoCompras
 	public Cliente Cliente { get; set; } = new Cliente();
 	public List<int> Ordenes { get; set; } = new List<int>();
 	public bool HaPagado { get; set; }
+
+	public void Agregar(int pizzaId) => Ordenes.Add(pizzaId);
+
+	public void Remover(int pos) => Ordenes.RemoveAt(pos);
 }
