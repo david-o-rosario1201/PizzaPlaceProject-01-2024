0199cb382961c672a09c0b349b73d1b6ebf0f1b2
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-30 11:16:04 -0400

    Agregué PrecioTotal y algunos using a la clase Estado

--- Shared/Estado.cs
+++ Shared/Estado.cs
@@ -1,8 +1,13 @@
-﻿namespace PizzaPlace.Shared;
+﻿using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace PizzaPlace.Shared;
 
 public class Estado
 {
 	public Menu Menu { get; } = new Menu();
 	public CarritoCompras Carrito { get; } = new CarritoCompras();
 	public UI UI { get; set; } = new UI();
+
+	public decimal PrecioTotal => Carrito.Ordenes.Sum(id => Menu.ObtenerPizza(id)!.Precio);
 }
