ca37c0b3d3dfa4f0de3abe4c409488f655a54439
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-30 11:14:45 -0400

    Agregué  validaciones a la clase Cliente

--- Shared/Cliente.cs
+++ Shared/Cliente.cs
@@ -1,9 +1,18 @@
-﻿namespace PizzaPlace.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Shared;
 
 public class Cliente
 {
+	[Key]
 	public int Id { get; set; }
+
+	[Required(ErrorMessage = "Debe ingresar su nombre")]
 	public string Nombre { get; set; } = default!;
+
+	[Required(ErrorMessage = "Debe ingresar una calle con el numero de casa")]
 	public string Calle { get; set; } = default!;
+
+	[Required(ErrorMessage = "Debe ingresar una ciudad")]
 	public string Ciudad { get; set; } = default!;
 }
